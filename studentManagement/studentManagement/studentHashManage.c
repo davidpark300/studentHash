46ad55b5e54626941b7e8331a991f89d7727416e
davidpark300 <davidpark300@naver.com>
2024-11-09 22:52:33 +0900

    CSV DATA Add Complete

--- studentManagement/studentManagement/studentHashManage.c
+++ studentManagement/studentManagement/studentHashManage.c
@@ -1,10 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define TABLE_SIZE 7
+#define TABLE_SIZE 2599999
+#define NAME_SIZE 100
+#define DATA_SIZE 256
+#define DATA_TYPES 5
 
 typedef struct {
 	int key;
+	char name[NAME_SIZE];
+	int phone_number;
 } element;
 
 struct list {
@@ -15,7 +22,7 @@ struct list {
 struct list* hash_table[TABLE_SIZE];
 
 int hash_function(int key) {
-	return key % TABLE_SIZE;
+	return key /* % TABLE_SIZE*/;
 }
 
 void hash_chain_add(element item, struct list* ht[]) {
@@ -30,7 +37,9 @@ void hash_chain_add(element item, struct list* ht[]) {
 		}
 	}
 	ptr = (struct list*)malloc(sizeof(struct list));
-	ptr->item = item;
+	ptr->item.phone_number = item.phone_number;
+	ptr->item.key = item.key;
+	strcpy(ptr->item.name, item.name);
 	ptr->link = NULL;
 
 	if (node_before)
@@ -52,34 +61,84 @@ void hash_chain_search(element item, struct list* ht[]) {
 	printf("키를 찾지 못했음\n");
 }
 
-void hash_chain_print(struct list* ht[]) {
+void hash_chain_print(int key) {
 	struct list* node;
-	int i;
+	int hash_value = hash_function(key);
 	printf("\n=======================================\n");
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		printf("[%d]->", i);
-		for (node = ht[i]; node; node = node->link) {
-			printf("%d->", node->item.key);
-		}
-		printf("\n");
+	if (hash_table[hash_value] == NULL) {
+		printf("등록된 학번이 없습니다.\n");
+		return;
 	}
+	for (node = hash_table[hash_value]; node; node = node->link) {
+		printf("%d, %s, 0%d", node->item.key, node->item.name, node->item.phone_number);
+	}
+	printf("\n");
 	printf("=======================================\n");
 }
 
-#define SIZE 5
+void csv_data_add() {
+	FILE* fp;
+	char str[DATA_SIZE];
+	element items;
+
+	if ((fp = fopen("studentData.csv", "r")) == NULL) {
+		printf("파일이 존재하지 않습니다.\n");
+		items.key = 0;
+		strcpy(items.name, "INVALID");
+		items.phone_number = 0;
+		fclose(fp);
+		return 0;
+	}
+
+	fgets(str, DATA_SIZE, fp);	// 파일에서 문자열을 가져오는 함수
+
+	char* sub = strtok(str, ",");
+
+	while(1) {
+		fgets(str, DATA_SIZE, fp);
+		sub = strtok(str, ",");
+		items.key = atoi(sub);
+		//printf("%d || ", atoi(sub));
+
+		sub = strtok(NULL, ",");
+		if (sub == NULL) {
+			items.key = 0;
+			printf("데이터 없음\n");
+			return 0;
+		}
+		strcpy(items.name, sub);
+		//printf("%s || ", sub);
+
+		sub = strtok(NULL, ",");
+		items.phone_number = atoi(sub);
+		//printf("%d || ", atoi(sub));
+		//printf("\n");
+
+		hash_chain_add(items, hash_table);
+
+	}
+}
 
 int main() {
-	int data[SIZE] = { 8,1,9,6,13 };
-	element e;
+	int choice;
+	int student_num;
 
-	for (int i = 0; i < SIZE; i++) {
-		e.key = data[i];
-		hash_chain_add(e, hash_table);
+	csv_data_add();
+
+	while (1) {
+		printf("선택지 : 1. 학생조회 2. 종료 => ");
+		scanf_s("%d", &choice);
+		if (choice == 1) {
+			printf("학번 입력 => ");
+			scanf_s("%d", &student_num);
+			hash_chain_print(student_num);
+		}
+		else if (choice == 2)
+			break;
+		else
+			printf("다시 입력하십시오\n");
 	}
-	hash_chain_print(hash_table);
-	for (int i = 0; i < SIZE; i++) {
-		e.key = data[i];
-		hash_chain_search(e, hash_table);
-	}
+
+	printf("\n\n");
 	return 0;
 }
